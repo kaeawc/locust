057894b8c08855e3d6ea573df991535fbd66ffb9
Jason Pearson <jason.d.pearson@gmail.com>
2016-08-08 17:54:53 -0400

    pep8 fixes

--- locust/test/test_stats.py
+++ locust/test/test_stats.py
@@ -66,7 +66,7 @@ class TestRequestStats(unittest.TestCase):
         self.assertEqual(self.s.num_failures, 1)
         self.assertEqual(self.s.avg_response_time, 420.5)
         self.assertEqual(self.s.median_response_time, 85)
-    
+
     def test_reset_min_response_time(self):
         self.s.reset()
         self.s.log(756, 0)
@@ -98,22 +98,22 @@ class TestRequestStats(unittest.TestCase):
         self.assertEqual(s.num_failures, 3)
         self.assertEqual(s.median_response_time, 38)
         self.assertEqual(s.avg_response_time, 43.2)
-    
+
     def test_serialize_through_message(self):
         """
-        Serialize a RequestStats instance, then serialize it through a Message, 
-        and unserialize the whole thing again. This is done "IRL" when stats are sent 
+        Serialize a RequestStats instance, then serialize it through a Message,
+        and unserialize the whole thing again. This is done "IRL" when stats are sent
         from slaves to master.
         """
         s1 = StatsEntry(self.stats, "test", "GET")
         s1.log(10, 0)
         s1.log(20, 0)
         s1.log(40, 0)
-        u1 = StatsEntry.unserialize(s1.serialize())
-        
+        StatsEntry.unserialize(s1.serialize())
+
         data = Message.unserialize(Message("dummy", s1.serialize(), "none").serialize()).data
         u1 = StatsEntry.unserialize(data)
-        
+
         self.assertEqual(20, u1.median_response_time)
 
 
@@ -121,63 +121,66 @@ class TestRequestStatsWithWebserver(WebserverTestCase):
     def test_request_stats_content_length(self):
         class MyLocust(HttpLocust):
             host = "http://127.0.0.1:%i" % self.port
-    
+
         locust = MyLocust()
         locust.client.get("/ultra_fast")
-        self.assertEqual(global_stats.get("/ultra_fast", "GET").avg_content_length, len("This is an ultra fast response"))
+        self.assertEqual(global_stats.get("/ultra_fast", "GET").avg_content_length,
+                         len("This is an ultra fast response"))
         locust.client.get("/ultra_fast")
-        self.assertEqual(global_stats.get("/ultra_fast", "GET").avg_content_length, len("This is an ultra fast response"))
-    
+        self.assertEqual(global_stats.get("/ultra_fast", "GET").avg_content_length,
+                         len("This is an ultra fast response"))
+
     def test_request_stats_no_content_length(self):
         class MyLocust(HttpLocust):
             host = "http://127.0.0.1:%i" % self.port
         l = MyLocust()
         path = "/no_content_length"
-        r = l.client.get(path)
-        self.assertEqual(global_stats.get(path, "GET").avg_content_length, len("This response does not have content-length in the header"))
-    
+        l.client.get(path)
+        self.assertEqual(global_stats.get(path, "GET").avg_content_length,
+                         len("This response does not have content-length in the header"))
+
     def test_request_stats_no_content_length_streaming(self):
         class MyLocust(HttpLocust):
             host = "http://127.0.0.1:%i" % self.port
         l = MyLocust()
         path = "/no_content_length"
-        r = l.client.get(path, stream=True)
+        l.client.get(path, stream=True)
         self.assertEqual(0, global_stats.get(path, "GET").avg_content_length)
-    
+
     def test_request_stats_named_endpoint(self):
         class MyLocust(HttpLocust):
             host = "http://127.0.0.1:%i" % self.port
-    
+
         locust = MyLocust()
         locust.client.get("/ultra_fast", name="my_custom_name")
         self.assertEqual(1, global_stats.get("my_custom_name", "GET").num_requests)
-    
+
     def test_request_stats_query_variables(self):
         class MyLocust(HttpLocust):
             host = "http://127.0.0.1:%i" % self.port
-    
+
         locust = MyLocust()
         locust.client.get("/ultra_fast?query=1")
         self.assertEqual(1, global_stats.get("/ultra_fast?query=1", "GET").num_requests)
-    
+
     def test_request_stats_put(self):
         class MyLocust(HttpLocust):
             host = "http://127.0.0.1:%i" % self.port
-    
+
         locust = MyLocust()
         locust.client.put("/put")
         self.assertEqual(1, global_stats.get("/put", "PUT").num_requests)
-    
+
     def test_request_connection_error(self):
         class MyLocust(HttpLocust):
             host = "http://localhost:1"
-        
+
         locust = MyLocust()
         response = locust.client.get("/meruem", timeout=0.1)
         self.assertEqual(response.status_code, 0)
         self.assertEqual(1, global_stats.get("/meruem", "GET").num_failures)
         self.assertEqual(0, global_stats.get("/meruem", "GET").num_requests)
-    
+
     def test_max_requests(self):
 
         class MyTaskSet(TaskSet):
@@ -190,26 +193,26 @@ class TestRequestStatsWithWebserver(WebserverTestCase):
             task_set = MyTaskSet
             min_wait = 1
             max_wait = 1
-            
+
         try:
             from locust.exception import StopLocust
             global_stats.clear_all()
             global_stats.max_requests = 2
-            
+
             l = MyLocust()
             self.assertRaises(StopLocust, lambda: l.task_set(l).run())
             self.assertEqual(2, global_stats.num_requests)
-            
+
             global_stats.clear_all()
             global_stats.max_requests = 2
             self.assertEqual(0, global_stats.num_requests)
-            
+
             l.run()
             self.assertEqual(2, global_stats.num_requests)
         finally:
             global_stats.clear_all()
             global_stats.max_requests = None
-    
+
     def test_max_requests_failed_requests(self):
 
         class MyTaskSet(TaskSet):
@@ -218,23 +221,23 @@ class TestRequestStatsWithWebserver(WebserverTestCase):
                 self.client.get("/ultra_fast")
                 self.client.get("/fail")
                 self.client.get("/fail")
-            
+
         class MyLocust(HttpLocust):
             host = "http://127.0.0.1:%i" % self.port
             task_set = MyTaskSet
             min_wait = 1
             max_wait = 1
-            
+
         try:
             from locust.exception import StopLocust
             global_stats.clear_all()
             global_stats.max_requests = 3
-            
+
             l = MyLocust()
             self.assertRaises(StopLocust, lambda: l.task_set(l).run())
             self.assertEqual(1, global_stats.num_requests)
             self.assertEqual(2, global_stats.num_failures)
-            
+
             global_stats.clear_all()
             global_stats.max_requests = 2
             self.assertEqual(0, global_stats.num_requests)
@@ -248,20 +251,23 @@ class TestRequestStatsWithWebserver(WebserverTestCase):
 
 
 class MyTaskSet(TaskSet):
+
     @task(75)
     def root_task(self):
         pass
-    
+
     @task(25)
     class MySubTaskSet(TaskSet):
+
         @task
         def task1(self):
             pass
+
         @task
         def task2(self):
             pass
 
-    
+
 class TestInspectLocust(unittest.TestCase):
     def test_get_task_ratio_dict_relative(self):
         ratio = get_task_ratio_dict([MyTaskSet])
@@ -270,7 +276,7 @@ class TestInspectLocust(unittest.TestCase):
         self.assertEqual(0.25, ratio["MyTaskSet"]["tasks"]["MySubTaskSet"]["ratio"])
         self.assertEqual(0.5, ratio["MyTaskSet"]["tasks"]["MySubTaskSet"]["tasks"]["task1"]["ratio"])
         self.assertEqual(0.5, ratio["MyTaskSet"]["tasks"]["MySubTaskSet"]["tasks"]["task2"]["ratio"])
-    
+
     def test_get_task_ratio_dict_total(self):
         ratio = get_task_ratio_dict([MyTaskSet], total=True)
         self.assertEqual(1.0, ratio["MyTaskSet"]["ratio"])
